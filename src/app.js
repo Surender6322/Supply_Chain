cdfba965e0c2ae538305c51a1dfa725cb589f7ea
alak urus <Surender.Kala@Cubastion.com>
2024-02-01 11:40:17 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+require("./db/index");
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the app");
+});
+
+app.listen(port, () => {
+  console.log(`The server is up and running on ${port}`);
+});
